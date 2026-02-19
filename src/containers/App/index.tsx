6c52933f121b85f029316d3213fbd0c72daa613f
Stanisław Zakrzewski <stanislaw.zakrzewski@htdevelopers.com>
2026-02-19 12:28:31 +0100

    Cleanup App

--- src/containers/App/index.tsx
+++ src/containers/App/index.tsx
@@ -1,22 +1,30 @@
 import {useEffect, useState} from "react";
-import "@aws-amplify/ui-react/styles.css";
-import {AppBar, Box, Drawer,CssBaseline, Toolbar, List, Divider, ListItem, ListItemButton, ListItemText, ListItemIcon, IconButton} from '@mui/material';
+import {Navigate, Route, Routes, useLocation, useNavigate} from 'react-router-dom';
+import {
+    Menu as MenuIcon,
+    BrowserUpdated as BrowserUpdatedIcon,
+    Public as PublicIcon,
+    Logout as LogoutIcon
+} from '@mui/icons-material';
+import {
+    AppBar,
+    Box,
+    CssBaseline,
+    Divider,
+    Drawer,
+    IconButton,
+    List,
+    ListItem,
+    ListItemButton,
+    ListItemIcon,
+    ListItemText,
+    Toolbar,
+} from '@mui/material';
+
 import DataDownloads from "../DataDownloads";
 import LocationAdminExplorer from "../LocationAdminExplorer";
-import LogoutIcon from '@mui/icons-material/Logout';
-import PublicIcon from '@mui/icons-material/Public';
-import BrowserUpdatedIcon from '@mui/icons-material/BrowserUpdated';
-import MenuIcon from '@mui/icons-material/Menu';
 import {useAppDispatch} from '../../hooks/redux';
 import {getUserProfile, logout} from "../../redux/app/thunk.ts";
-import {
-    Link,
-    Route,
-    Routes,
-    Navigate,
-    useLocation,useNavigate
-
-} from 'react-router-dom';
 
 
 export default function App() {
@@ -37,26 +45,20 @@ export default function App() {
     const menuList = [
         {
             text: 'Data downloads',
-            icon: <BrowserUpdatedIcon />,
-            to: { pathname: '/data-downloads', search: '' },
-            displayCheck: (): boolean => true,
+            icon: <BrowserUpdatedIcon/>,
+            to: '/data-downloads',
         },
         {
             text: 'Location admin explorer',
-            icon: <PublicIcon />,
-            to: { pathname: '/location-admin-explorer', search: '' },
-            displayCheck: (): boolean => true,
+            icon: <PublicIcon/>,
+            to: '/location-admin-explorer',
         },
     ]
 
     useEffect(() => {
-        const menuIndex = menuList.findIndex((menuItem) => {
-            const pathname =
-                typeof menuItem.to === 'string'
-                    ? menuItem.to
-                    : menuItem.to.pathname;
-            return pathname === location.pathname;
-        });
+        const menuIndex = menuList.findIndex((menuItem) => (
+            menuItem.to === location.pathname
+        ));
         setSelectedMenuIndex(menuIndex);
     }, [location.pathname, menuList]);
 
@@ -65,12 +67,11 @@ export default function App() {
     console.log(menuList)
 
 
-
     return (
         <>
-            <Box sx={{ display: 'flex' }}>
-                <CssBaseline />
-                <AppBar position="fixed" sx={{ zIndex: (theme) => theme.zIndex.drawer + 1 }}>
+            <Box sx={{display: 'flex'}}>
+                <CssBaseline/>
+                <AppBar position="fixed" sx={{zIndex: (theme) => theme.zIndex.drawer + 1}}>
                     <Toolbar>
                         <IconButton
                             aria-label="toggle drawer"
@@ -80,7 +81,7 @@ export default function App() {
                             size="large"
                             style={{color: 'white'}}
                         >
-                            <MenuIcon />
+                            <MenuIcon/>
                         </IconButton>
                     </Toolbar>
                 </AppBar>
@@ -90,47 +91,41 @@ export default function App() {
                     sx={{
                         width: drawerWidth,
                         flexShrink: 0,
-                        [`& .MuiDrawer-paper`]: { width: drawerWidth, boxSizing: 'border-box' },
+                        [`& .MuiDrawer-paper`]: {width: drawerWidth, boxSizing: 'border-box'},
                     }}
                 >
-                    <Toolbar />
-                    <Box sx={{ overflow: 'auto' }}>
+                    <Toolbar/>
+                    <Box sx={{overflow: 'auto'}}>
                         <List>
                             {menuList.map(
                                 (item, index) =>
-                                    item.displayCheck() && (
-
-                                            <ListItem
-                                                key={item.text}
-
-                                            >
-
-                                                <ListItemButton  onClick={() => navigate(item.to, { state: { lastLocation: location.pathname } })}selected={selectedMenuIndex === index}>
-                                                <ListItemIcon>{item.icon}</ListItemIcon>
-
-                                                <ListItemText primary={item.text}  />
-                                                </ListItemButton>
-
-                                            </ListItem>
-
-                                    ),
+                                    <ListItem
+                                        key={item.text}
+                                    >
+                                        <ListItemButton
+                                            onClick={() => navigate(item.to, {state: {lastLocation: location.pathname}})}
+                                            selected={selectedMenuIndex === index}>
+                                            <ListItemIcon>{item.icon}</ListItemIcon>
+                                            <ListItemText primary={item.text}/>
+                                        </ListItemButton>
+                                    </ListItem>
                             )}
                         </List>
-                        <Divider />
+                        <Divider/>
                         <List>
                             <ListItem>
                                 <ListItemButton onClick={handleLogout}>
                                     <ListItemIcon>
-                                        <LogoutIcon />
+                                        <LogoutIcon/>
                                     </ListItemIcon>
-                                    <ListItemText primary={'Logout'} />
+                                    <ListItemText primary={'Logout'}/>
                                 </ListItemButton>
                             </ListItem>
                         </List>
                     </Box>
                 </Drawer>
-                <Box component="main" sx={{ flexGrow: 1, p: 3 }}>
-                    <Toolbar />
+                <Box component="main" sx={{flexGrow: 1, p: 3}}>
+                    <Toolbar/>
                     <Routes>
                         <Route path="/data-downloads" element={<DataDownloads/>}/>
                         <Route path="/location-admin-explorer" element={<LocationAdminExplorer/>}/>
@@ -148,46 +143,6 @@ export default function App() {
                     </Routes>
                 </Box>
             </Box>
-            {/*<AppBar position="fixed" elevation={0}>*/}
-            {/*    <Toolbar style={{marginLeft: 'auto', marginRight: '0px'}}>*/}
-            {/*        <Button onClick={toggleDrawer(!drawerOpen)} variant="contained" color="secondary"*/}
-            {/*                style={{height: '100%', width: '150px'}}>Open drawer</Button>*/}
-            {/*        <Button onClick={handleLogout} variant="contained" color="error"*/}
-            {/*                style={{height: '100%', width: '150px'}}>Sign Out</Button>*/}
-            {/*    </Toolbar>*/}
-            {/*</AppBar>*/}
-            {/*<Drawer open={drawerOpen} onClose={toggleDrawer(false)} variant="persistent"*/}
-            {/*        anchor="left"      sx={{*/}
-            {/*    width: drawerWidth,*/}
-            {/*    flexShrink: 0,*/}
-            {/*    [`& .MuiDrawer-paper`]: { width: drawerWidth, boxSizing: 'border-box' },*/}
-            {/*}}>*/}
-            {/*    {DrawerList}*/}
-            {/*</Drawer>*/}
-            {/*<Routes>*/}
-            {/*    <Route path="/data-downloads" element={<DataDownloads/>}/>*/}
-            {/*    <Route*/}
-            {/*        path="/"*/}
-            {/*        element={*/}
-            {/*            <Navigate*/}
-            {/*                to={{*/}
-            {/*                    pathname: '/data-downloads',*/}
-            {/*                }}*/}
-            {/*                replace*/}
-            {/*            />*/}
-            {/*        }*/}
-            {/*    />*/}
-            {/*</Routes>*/}
-            {/*/!*<Grid container spacing={2} style={{width: '100%'}}>*!/*/}
-            {/*/!*    <Grid size={10}></Grid>*!/*/}
-            {/*/!*    <Grid size={2}>*!/*/}
-            {/*/!*        <Button onClick={handleLogout} variant="contained" color="error"*!/*/}
-            {/*/!*                style={{height: '100%', width: '100%'}}>Sign*!/*/}
-            {/*/!*            Out</Button>*!/*/}
-            {/*/!*    </Grid>*!/*/}
-            {/*/!*    <DataDownloads/>*!/*/}
-            {/*/!*</Grid>*!/*/}
         </>
-
     );
 }
