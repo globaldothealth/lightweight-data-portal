e3f665e04405d55121bcac587892ab243af6c4b4
Stanisław Zakrzewski <stanislaw.zakrzewski@htdevelopers.com>
2026-03-02 13:48:41 +0100

    Thunk tests

--- src/redux/dataDownloads/thunk.ts
+++ src/redux/dataDownloads/thunk.ts
@@ -10,17 +10,17 @@ export const getFilesFromS3Folder = createAsyncThunk<S3File[],
     'dataDownloads/getFilesFromS3Folder',
     async (data, {rejectWithValue}) => {
         try {
-            const result: any = await list({path: `public/${data.s3Folder}/`});
-            const files: S3File[] = (result.items.map((file: { path: string }) => ({
+            const result = await list({path: `public/${data.s3Folder}/`});
+            const files: S3File[] = (result.items.map((file) => ({
                 filename: file.path,
-                name: file.path.split('/').pop(),
+                name: file.path.split('/').pop() || '',
             })).filter((file: { name: string; filename: string }) => file.name !== ''));
             if (files.length === 0) {
                 return rejectWithValue('No files found in the specified S3 folder.');
             }
             return files;
-        } catch (error: any) {
-            const message = error?.message ? String(error.message) : 'An error occurred while listing S3 files.';
+        } catch (error: unknown) {
+            const {message} = error as Error;
             return rejectWithValue(`Error fetching files from S3: ${message}`);
         }
     },
@@ -42,8 +42,8 @@ export const handleDownload = createAsyncThunk<void,
             const link = await getUrl({path: data.s3FileKey});
 
             window.open(link.url.toString(), '_blank');
-        } catch (error: any) {
-            const message = error?.message ? String(error.message) : 'An error occurred while downloading S3 file.';
+        } catch (error: unknown) {
+            const {message} = error as Error;
             return rejectWithValue(`Error downloading file from S3: ${message}`);
         }
     },
