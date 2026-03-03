05da8a01e97547843e6442b9088e0a5e5f877e06
Stanisław Zakrzewski <stanislaw.zakrzewski@htdevelopers.com>
2026-03-03 15:43:53 +0100

    Fix eslint errors

--- eslint.config.js
+++ eslint.config.js
@@ -5,7 +5,7 @@ import reactRefresh from 'eslint-plugin-react-refresh'
 import tseslint from 'typescript-eslint'
 
 export default tseslint.config(
-  { ignores: ['dist'] },
+  { ignores: ['dist', '.amplify', 'amplify'] },
   {
     extends: [js.configs.recommended, ...tseslint.configs.recommended],
     files: ['**/*.{ts,tsx,js,jsx}'],
@@ -28,4 +28,3 @@ export default tseslint.config(
     },
   },
 )
-
