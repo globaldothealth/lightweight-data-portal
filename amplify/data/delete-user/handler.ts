5ac7a345466de76bcc909f372073f9c51eb4cf72
Stanisław Zakrzewski <stanislaw.zakrzewski@htdevelopers.com>
2026-04-07 14:39:29 +0200

    Fix user and userprofile mixup in dengue, block removing admin users from backend

--- amplify/data/delete-user/handler.ts
+++ amplify/data/delete-user/handler.ts
@@ -2,6 +2,7 @@ import type {Schema} from "../resource"
 import {env} from "$amplify/env/delete-user"
 import {
     AdminDeleteUserCommand,
+    AdminListGroupsForUserCommand,
     CognitoIdentityProviderClient,
 } from "@aws-sdk/client-cognito-identity-provider"
 
@@ -10,10 +11,21 @@ const client = new CognitoIdentityProviderClient()
 
 export const handler: Handler = async (event) => {
     const {username} = event.arguments
+
+    const listGroupsCommand = new AdminListGroupsForUserCommand({
+        Username: username,
+        UserPoolId: env.AMPLIFY_AUTH_USERPOOL_ID,
+    })
+    const groupsResponse = await client.send(listGroupsCommand)
+    const isAdmin = groupsResponse.Groups?.some((group) => group.GroupName === "ADMINS")
+
+    if (isAdmin) {
+        throw new Error("User cannot be deleted because they are in the ADMINS group.")
+    }
+
     const command = new AdminDeleteUserCommand({
         Username: username,
         UserPoolId: env.AMPLIFY_AUTH_USERPOOL_ID,
     })
     return await client.send(command)
 }
-
