7c4d1f2c4cd9b5e55ca7d7b06e4f8b1babad97a2
Stanisław Zakrzewski <stanislaw.zakrzewski@htdevelopers.com>
2026-04-16 13:10:32 +0200

    Update logic behind getUsers

--- amplify/data/get-users/handler.ts
+++ amplify/data/get-users/handler.ts
@@ -2,7 +2,8 @@ import type { Schema } from "../resource"
 import { env } from "$amplify/env/get-users"
 import {
     ListUsersCommand,
-    AdminListGroupsForUserCommand,
+    ListGroupsCommand,
+    ListUsersInGroupCommand,
     CognitoIdentityProviderClient,
 } from "@aws-sdk/client-cognito-identity-provider"
 
@@ -15,24 +16,41 @@ export const handler: Handler = async () => {
     })
     const { Users } = await client.send(listUsersCommand)
 
+    const listGroupsCommand = new ListGroupsCommand({
+        UserPoolId: env.AMPLIFY_AUTH_USERPOOL_ID,
+    })
+    const { Groups } = await client.send(listGroupsCommand)
+
+    const userGroupsMap = new Map<string, string[]>()
+
+    for (const group of Groups || []) {
+        const groupName = group.GroupName!
+        const listUsersInGroupCommand = new ListUsersInGroupCommand({
+            UserPoolId: env.AMPLIFY_AUTH_USERPOOL_ID,
+            GroupName: groupName,
+        })
+        const groupUsers = await client.send(listUsersInGroupCommand)
+
+        for (const u of groupUsers.Users || []) {
+            const username = u.Username!
+            if (!userGroupsMap.has(username)) {
+                userGroupsMap.set(username, [])
+            }
+            userGroupsMap.get(username)!.push(groupName)
+        }
+    }
+
     const usersWithGroups = []
     for (const u of Users || []) {
         const email = u.Attributes?.find(attribute => attribute.Name === 'email')?.Value || ''
         const username = u.Username!
 
-        const groupResponse = await client.send(new AdminListGroupsForUserCommand({
-            UserPoolId: env.AMPLIFY_AUTH_USERPOOL_ID,
-            Username: username,
-        }))
-        const groups = (groupResponse.Groups || []).map(group => group.GroupName)
-
         usersWithGroups.push({
             username,
             email,
-            groups
+            groups: userGroupsMap.get(username) || []
         })
     }
 
     return usersWithGroups as any
 }
-
