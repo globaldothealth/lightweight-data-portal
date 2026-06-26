c366015a517929d8824d593a2d1d8f5ea8cb576b
Stanisław Zakrzewski <stanislaw.zakrzewski@htdevelopers.com>
2026-06-26 16:58:27 +0200

    Tests for map aggregetion redux done

---
+++ src/redux/mapDataAggregation/tests/selectors.test.ts
@@ -0,0 +1,32 @@
+import {describe, it, expect, vi} from 'vitest';
+import {selectScheduleConfigs, selectIsLoading, selectError} from '../selectors.ts';
+import {RootState} from '../../store.ts';
+
+vi.mock('aws-amplify/data', () => ({
+    generateClient: vi.fn(),
+}));
+
+describe('MapDataAggregation Selectors', () => {
+    const scheduleConfig1 = { id: '1', scheduledExpression: "0 0 * * *", outbreakName: 'Ebola BVD', enabled: true };
+    const scheduleConfig2 = { id: '1', scheduledExpression: "0 0 * * *", outbreakName: 'Ebola BVD', enabled: true };
+    const errorMessage = 'error message';
+    const mockState = {
+        mapDataAggregation: {
+            scheduleConfigs: [scheduleConfig1, scheduleConfig2],
+            isLoading: true,
+            error: errorMessage
+        }
+    } as unknown as RootState;
+
+    it('should select schedule configs', () => {
+        expect(selectScheduleConfigs(mockState)).toEqual([scheduleConfig1, scheduleConfig2]);
+    });
+
+    it('should select isLoading', () => {
+        expect(selectIsLoading(mockState)).toBe(true);
+    });
+
+    it('should select error', () => {
+        expect(selectError(mockState)).toBe(errorMessage);
+    });
+});
