febea61dd3086bbfee0b5af95926f30965b4ac9f
Stanisław Zakrzewski <stanislaw.zakrzewski@htdevelopers.com>
2026-06-26 13:12:07 +0200

    Remove ability to add disabled schedule

--- src/containers/MapDataAggregation/index.tsx
+++ src/containers/MapDataAggregation/index.tsx
@@ -15,12 +15,10 @@ import {
     Paper,
     Select,
     SelectChangeEvent,
-    Switch,
     TextField,
     Tooltip,
     Typography,
     CircularProgress,
-    FormControlLabel,
 } from '@mui/material';
 import DeleteIcon from '@mui/icons-material/Delete';
 
@@ -48,7 +46,6 @@ const MapDataAggregation = () => {
     const error = useAppSelector(selectError);
 
     const [outbreakName, setOutbreakName] = useState<OutbreakName | ''>('');
-    const [enabled, setEnabled] = useState(true);
     const [scheduleType, setScheduleType] = useState<ScheduleType>('rate');
     const [rateValue, setRateValue] = useState('30');
     const [rateUnit, setRateUnit] = useState('minutes');
@@ -74,11 +71,10 @@ const MapDataAggregation = () => {
             createScheduleConfig({
                 scheduleExpression: buildScheduleExpression(),
                 outbreakName,
-                enabled,
+                enabled: true,
             }),
         );
         setOutbreakName('');
-        setEnabled(true);
         setRateValue('30');
         setRateUnit('minutes');
         setCronExpression('0 0 * * ? *');
@@ -146,8 +142,8 @@ const MapDataAggregation = () => {
                                                 {config.outbreakName}
                                             </Typography>
                                             <Chip
-                                                label={config.enabled ? 'Enabled' : 'Disabled'}
-                                                color={config.enabled ? 'success' : 'default'}
+                                                label='Enabled'
+                                                color='success'
                                                 size="small"
                                             />
                                         </Box>
@@ -243,16 +239,6 @@ const MapDataAggregation = () => {
                                 helperText="Format: minute hour day-of-month month day-of-week year (e.g. 0 0 * * ? *)"
                             />
                         )}
-
-                        <FormControlLabel
-                            control={
-                                <Switch
-                                    checked={enabled}
-                                    onChange={(e) => setEnabled(e.target.checked)}
-                                />
-                            }
-                            label="Enabled"
-                        />
 
                         <Button
                             type="submit"
